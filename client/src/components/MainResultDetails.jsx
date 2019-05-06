b620733379f14f79e10be541f86eb62c36a83e4c
Evan Kaplan <evankaplan@fb.com>
2019-05-06 14:06:51 -0700

    Refactor main transaction to show multiple inputs and outputs

--- client/src/components/MainResultDetails.jsx
+++ client/src/components/MainResultDetails.jsx
@@ -4,9 +4,20 @@ const MainResultDetails = ({ mainResult }) => (
   <div className="results-container">
     <h4>Transaction Details </h4>
     <div className="main-transaction">
+      <h5>Overview</h5>
+      {mainResult.txName && (
+        <div className="line-item">
+          <div>
+            Transaction Name:
+          </div>
+          <div>
+            {mainResult.txName}
+          </div>
+        </div>
+      )}
       <div className="line-item">
         <div>
-          Transaction Id:
+          Transaction Hash:
         </div>
         <div>
           {mainResult.txHash}
@@ -22,44 +33,40 @@ const MainResultDetails = ({ mainResult }) => (
       </div>
     </div>
     <div className="transaction-details">
-      {
-        mainResult.inputs.map((transaction, idx) => (
-          <Fragment>
-            <div className="line-item">
-              <div>
-                From Address:
-              </div>
-              <div>
-                {transaction.prev_out.addr}
-              </div>
-            </div>
-            <div className="line-item">
-              <div>
-                To Address:
-              </div>
-              <div>
-                {mainResult.outputs[0].addr}
-              </div>
-            </div>
-            <div className="line-item">
-              <div>
-                Amount Sent: 
-              </div>
-              <div>
-                {`${transaction.prev_out.value / 100000000} bitcoin`}
-              </div>
-            </div>
-            <div className="line-item">
-              <div>
-                Fee: 
-              </div>
-              <div>
-                {`${(transaction.prev_out.value - mainResult.outputs[0].value) / 100000000}  bitcoin`}
-              </div>
-            </div>
-          </Fragment>
-        ))
-      }
+      <h5>Inputs</h5>
+      {mainResult.inputs.map(transaction => (
+        <div className="line-item">
+          <div>
+            Address:
+          </div>
+          <div>
+            {transaction.prev_out.addr}
+          </div>
+          <div>
+            Amount Sent:
+          </div>
+          <div>
+            {`${transaction.prev_out.value / 100000000} bitcoin`}
+          </div>
+        </div>
+      ))}
+      <h5>Outputs</h5>
+      {mainResult.outputs.map(transaction => (
+        <div className="line-item">
+          <div>
+            Address:
+          </div>
+          <div>
+            {transaction.addr}
+          </div>
+          <div>
+            Amount Sent:
+          </div>
+          <div>
+            {transaction.value ? `${transaction.value / 100000000} bitcoin` : 'No output'}
+          </div>
+        </div>
+      ))}
     </div>
   </div>
 );
